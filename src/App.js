8ea61a49b65004c34595e17c1ca91bff1c8b22c3
Paulo-Viana <paulodev1612@gmail.com>
2021-11-23 16:16:44 -0300

    Estruturação/Estilização inicial dos componentes Header, Footer, Home, Login e suas respectivas rotas.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home'
+import Login from './components/Login/Login'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Insta Dogs</h1>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
